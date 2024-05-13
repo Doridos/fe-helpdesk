a04f979ccf2831133b2371850b45b37bb6c3cc96
Vladyslav Babyč <babycvla@fel.cvut.cz>
2024-05-13 16:34:13 +0200

    add changes to config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    host: '192.168.130.58',
-    port: 80,
-  },
+
 })
